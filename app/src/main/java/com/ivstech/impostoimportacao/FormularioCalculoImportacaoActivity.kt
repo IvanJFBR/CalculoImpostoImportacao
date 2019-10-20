09f2db818967955e43a3a2a0df2013bb928e62a9
Ivan Junior <ivanjfbr@gmail.com>
2019-10-20 00:50:33 -0300

    Criação do layout do formulário com aplicações do Material Design

---
+++ app/src/main/java/com/ivstech/impostoimportacao/FormularioCalculoImportacaoActivity.kt
@@ -0,0 +1,12 @@
+package com.ivstech.impostoimportacao
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormularioCalculoImportacaoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_calculo_importacao)
+    }
+}
